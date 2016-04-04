5edac0815fc84cb4ea5a6a8304f47a70a5f9db27
Chris Blake <chrisrblake93@gmail.com>
2016-04-03 20:56:26 -0400

    apm82181: add support for the apm821xx device target
    This adds a new target for PowerPC APM821xx (464-based) boards,
    as well as adds support for the booke-wdt watchdog package.
    
    Signed-off-by: Chris Blake <chrisrblake93@gmail.com>

--- package/kernel/linux/modules/other.mk
+++ package/kernel/linux/modules/other.mk
@@ -517,7 +517,7 @@ $(eval $(call KernelPackage,wdt-orion))
 define KernelPackage/booke-wdt
   SUBMENU:=$(OTHER_MENU)
   TITLE:=PowerPC Book-E Watchdog Timer
-  DEPENDS:=@(TARGET_mpc85xx||TARGET_ppc40x||TARGET_ppc44x)
+  DEPENDS:=@(TARGET_apm821xx||TARGET_mpc85xx||TARGET_ppc40x||TARGET_ppc44x)
   KCONFIG:=CONFIG_BOOKE_WDT
   FILES:=$(LINUX_DIR)/drivers/$(WATCHDOG_DIR)/booke_wdt.ko
   AUTOLOAD:=$(call AutoLoad,50,booke_wdt,1)
