cdab0f1b7afe848aa594890a97120f30acc13d69
Christian Lamparter <chunkeey@googlemail.com>
2016-05-22 23:18:58 +0200

    apm82181: add support for the Netgear Centria N900 WNDR4700/WNDR4720
    This patch adds support for Netgear Centria N900 WNDR4700/WNDR4720
    
    hardware highlights:
    
    CPU:	AMCC PowerPC APM82181 Rev. E at 1000 MHz (PLB=166, OPB=83, EBC=83 MHz)
    	Security support, Boot ROM Location NAND wo/ECC 2k page (8 bits)
    	32 kB I-Cache 32 kB D-Cache, 256 kB L2-Cache, 32 kB OnChip Memory
    Board:	AMCC APM82181 Evaluation Board, PCIE0/SATA1, 1*USB OTG
    DRAM:	256 MB (ECC not enabled, 500 Mb/s, 32-bit, CL3)
    NAND:	128 MiB (SLC, erase size: 128 KiB, page size: 2048, OOB size: 64)
    ETH:	Atheros AR8327N Gigabit Switch (4 x LAN, 1 x WAN)
    USB:	2 x 3.0 (Renesas uPD720202K8-711-BAA-A)
    SATA:	1 x SATA-II 3.5" Hard Drive Bay for HDDs (DesignWare SATA).
    WLAN1:	Atheros AR9380 5GHz 802.11an 3:3x3
    WLAN2:	Atheros AR9581 2.4GHz 802.11bgn 3:3x3
    SDCARD:	GL827L SD/MMC/MS Flash Card Reader (on internal dwc2 USB 2.0 host)
    I2C:	GMT G781 (i2c-0 @ 0x4d - lm90 compatible temperature sensor)
    	TC654    (i2c-0 @ 0x1b - Dual PWM fan Speed controller)
    
    WARNING: The serial port needs a TTL/RS-232 v3.3 level converter!
    
    INFO: Since this device only has a NAND chip. I opted for going with
    root.squashfs in a UBI volume. There's no squashfs/jffs2 image.
    
    This target produces three images.
      a. netgear factory image
         This image can be used to flash the Netgear WNDR4700 via the
         firmware recovery mechanism and the web admin site.
    
         The bootloader can be instructed to do a firmware recovery via the
         # fw_recovery
         command. It will start a tftp server and listen on 192.168.1.1
         (the ipaddr variable in u-boot) for incoming, binary tftp clients.
         The firmware recovery mechanism is also started if any of the flash
         content which contains the kernel, device-tree definitions or the
         (fake)rootfs fails to verify or load.
    
      b. sysupgrade.tar image for sysupgrade
         An sysupgrade will replace the entire current LEDE installation
         with a newer version. This does include the kernel and the ubi rootfs
         partition. The configuration can be carried over automatically as well
         if desired.
    
         simply copy the sysupgrade.tar to a the WNDR4700 running LEDE and run:
    
         root@lede:~# sysupgrade sysupgrade.tar
    
         and let it reboot.
    
         Note: The devicetree flash area is NOT updated. Until the devicetree
    	   definition is stable, this can lead to all sorts of hardware
    	   detection problems! So make sure, if you experience issues: try
    	   the fw_recovery. If you are unsure whenever this affects you:
    	   test if you can reproduce your issue with the initramfs method.
    	   As it will always have up-to-date device-tree definitions.
    
      c. initramfs image for TFTP (for development and testing)
         To use the initramfs method, follow the following steps:
           1) Move the "lede-apm821xx-netgear-WNDR4700-initramfs-kernel.bin"
    	  file to to the root directory of your TFTP server.
    
           2) rename it to wndr4700.bin
    
           3) On the WNDR4700 - Hit Enter during u-boot and insert:
    	  # tftp 400000 wndr4700.bin; run addtty; bootm 400000 -
    
         This will boot the LEDE image.
    
         Note: The default tftp server is 192.168.1.7, if you want to change it:
         # setenv serverip 192.168.8.7;
    
         Note2: The default address for the WNDR4700 is 192.168.1.1:
         # setenv ipaddr 192.168.8.8;
    
         Note: Connect you tftp server on the last LAN port (not the WAN)
    
    Signed-off-by: Christian Lamparter <chunkeey@googlemail.com>

---
+++ target/linux/apm821xx/netgear/target.mk
@@ -0,0 +1,8 @@
+BOARDNAME:=Netgear Centria N900 WNDR4700/WNDR4720
+FEATURES += nand pcie usb ramdisk squashfs
+DEFAULT_PACKAGES += partx-utils block-mount kmod-dm kmod-md-mod e2fsprogs \
+		    badblocks swconfig renesas-firmware
+
+define Target/Description
+	Build firmware images for Netgear Centria N900 WNDR4700/WNDR4720 APs.
+endef
