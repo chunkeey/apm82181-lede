ff04d6df148377a8c0742d747e6735df1a4b6702
Christian Lamparter <chunkeey@gmail.com>
2019-03-09 11:08:27 +0100

    ipq40xx: convert Cisco Meraki MR33 to DSA
    This patch converts the Cisco Meraki MR33 to use ipqess + qca8k's DSA.
    
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
+++ target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
@@ -24,8 +24,7 @@ preinit_set_mac_address() {
 		ip link set dev eth1 address $(macaddr_add "${base_mac}" 1)
 		;;
 	meraki,mr33)
-		mac_lan=$(get_mac_binary "/sys/bus/i2c/devices/0-0050/eeprom" 0x66)
-		[ -n "$mac_lan" ] && ip link set dev eth0 address "$mac_lan"
+		lan_mac=$(get_mac_binary "/sys/bus/i2c/devices/0-0050/eeprom" 0x66)
 		;;
 	zyxel,nbg6617)
 		base_mac=$(cat /sys/class/net/eth0/address)
