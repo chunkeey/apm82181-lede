ae64aa5780b41bcab41e212cef0b53db269bbcd8
Christian Lamparter <chunkeey@gmail.com>
2019-03-09 18:08:27 +0100

    ipq40xx: convert ASUS RT-AC58U to DSA
    This patch converts the ASUS RT-AC58U to ipqess ethernet + qca8k's DSA.
    
    Signed-off-by: Christian Lamparter <chunkeey@gmail.com>

--- target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
+++ target/linux/ipq40xx/base-files/lib/preinit/05_set_iface_mac_ipq40xx.sh
@@ -3,11 +3,20 @@
 . /lib/functions.sh
 
 preinit_set_mac_address() {
+	local wan_mac
+	local lan14_mac
+	local lan_mac
+
 	case $(board_name) in
 	asus,map-ac2200)
 		base_mac=$(mtd_get_mac_binary_ubi Factory 0x1006)
 		ip link set dev eth0 address $(macaddr_add "$base_mac" +1)
 		ip link set dev eth1 address $(macaddr_add "$base_mac" +3)
+		;;
+	asus,rt-ac58u)
+		CI_UBIPART=UBI_DEV
+		wan_mac=$(mtd_get_mac_binary_ubi Factory 20486)
+		lan14_mac=$(mtd_get_mac_binary_ubi Factory 4102)
 		;;
 	linksys,ea8300)
 		base_mac=$(mtd_get_mac_ascii devinfo hw_mac_addr)
@@ -23,6 +32,15 @@ preinit_set_mac_address() {
 		ip link set dev eth0 address $(macaddr_add "$base_mac" +2)
 		ip link set dev eth1 address $(macaddr_add "$base_mac" +3)
 	esac
+
+	[ -n "$lan_mac" ] && ip link set dev lan address "$lan_mac"
+	[ -n "$wan_mac" ] && ip link set dev wan address "$wan_mac"
+	[ -n "$lan14_mac" ] && {
+		ip link set dev lan1 address "$lan14_mac"
+		ip link set dev lan2 address "$lan14_mac"
+		ip link set dev lan3 address "$lan14_mac"
+		ip link set dev lan4 address "$lan14_mac"
+	}
 }
 
 boot_hook_add preinit_main preinit_set_mac_address
